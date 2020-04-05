89c8087b8de40368bf00c7999344b5c5cdfc4ba1
toni789 <43863327+toni789@users.noreply.github.com>
2020-04-05 19:50:20 +0200

    NBack: added activity

---
+++ nback/src/main/java/ch/h/nback/NBackActivity.kt
@@ -0,0 +1,12 @@
+package ch.h.nback
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NBackActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_n_back)
+    }
+}
