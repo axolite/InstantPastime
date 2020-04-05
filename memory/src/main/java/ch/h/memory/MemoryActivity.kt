8bf9eb1586155d080d0fb36afab095f18ed41059
toni789 <43863327+toni789@users.noreply.github.com>
2020-04-05 19:46:42 +0200

    Memory: added activity

---
+++ memory/src/main/java/ch/h/memory/MemoryActivity.kt
@@ -0,0 +1,12 @@
+package ch.h.memory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memory)
+    }
+}
