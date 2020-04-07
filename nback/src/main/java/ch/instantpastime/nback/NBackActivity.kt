5cfe7737e643b159f79622874539a783ddb16a1c
toni789 <43863327+toni789@users.noreply.github.com>
2020-04-07 20:16:12 +0200

    NBack: load fragment from activity

--- nback/src/main/java/ch/instantpastime/nback/NBackActivity.kt
+++ nback/src/main/java/ch/instantpastime/nback/NBackActivity.kt
@@ -2,11 +2,22 @@ package ch.instantpastime.nback
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ch.instantpastime.nback.fragments.NBackFragment
 
 class NBackActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nback)
+
+        configureAndShowNBackFragment()
+    }
+
+    private fun configureAndShowNBackFragment() {
+        if (supportFragmentManager.findFragmentById(R.id.nback_fragment_container) == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.nback_fragment_container, NBackFragment())
+                .commit()
+        }
     }
 }
