00dba72ef6edcc12a70c05a44c68262783facbd1
toni789 <43863327+toni789@users.noreply.github.com>
2020-04-09 11:50:02 +0200

    NBack: added sounds

--- nback/src/main/java/ch/instantpastime/nback/core/NBackSound.kt
+++ nback/src/main/java/ch/instantpastime/nback/core/NBackSound.kt
@@ -5,8 +5,10 @@ import android.content.res.AssetFileDescriptor
 import android.media.MediaPlayer
 import android.util.Log
 import java.lang.Exception
+import java.nio.file.Path
+import java.nio.file.Paths
 
-class NBackSound {
+class NBackSound() {
 
     private var _player: MediaPlayer? = null
     private val _sounds: List<String> = listOf("c.wav", "h.wav", "k.wav", "l.wav", "q.wav", "r.wav", "s.wav", "t.wav")
@@ -24,29 +26,36 @@ class NBackSound {
         if (index in 0 until letterCount) {
             _player?.apply {
                 reset()
-                val afd = openAsset(context, _sounds[index])
-                if (afd != null) {
+                val soundPath: Path? = Paths.get(SoundFolderName, _sounds[index])
+                if (soundPath != null) {
+                    val afd = openAsset(context, soundPath.toString())
+                    if (afd != null) {
 
-                    // Set the sound file to play.
-                    try {
-                        setDataSource(afd)
-                    } catch (ex: Exception) {
-                        Log.d(NBackSound::class.java.simpleName, "Error setting player source", ex)
-                    }
+                        // Set the sound file to play.
+                        try {
+                            setDataSource(afd)
+                        } catch (ex: Exception) {
+                            Log.d(
+                                NBackSound::class.java.simpleName,
+                                "Error setting player source",
+                                ex
+                            )
+                        }
 
-                    // Set an error listener.
-                    setOnErrorListener { mp, what, extra ->
-                        Log.d(NBackSound::class.java.simpleName, "Player error occurred")
-                        true
-                    }
+                        // Set an error listener.
+                        setOnErrorListener { mp, what, extra ->
+                            Log.d(NBackSound::class.java.simpleName, "Player error occurred")
+                            true
+                        }
 
-                    // Prepare and play when player is prepared.
-                    prepareAsync()
-                    setOnPreparedListener {
-                        it.start()
-                        Log.d(NBackSound::class.java.simpleName, "Player is prepared")
+                        // Prepare and play when player is prepared.
+                        prepareAsync()
+                        setOnPreparedListener {
+                            it.start()
+                            Log.d(NBackSound::class.java.simpleName, "Player is prepared")
+                        }
+                        return true
                     }
-                    return true
                 }
             }
         }
@@ -64,6 +73,10 @@ class NBackSound {
             Log.d(NBackSound::class.simpleName, "Error loading sound asset '$fileName'", ex)
             null
         }
+    }
+
+    companion object {
+        val SoundFolderName = "letters"
     }
 
 }
