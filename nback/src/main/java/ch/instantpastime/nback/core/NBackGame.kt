0a027720105f70c7ae4c9cd8636d3449d8f6ad39
toni789 <43863327+toni789@users.noreply.github.com>
2020-04-10 15:50:45 +0200

    Extracted random into common NBackDraw

--- nback/src/main/java/ch/instantpastime/nback/core/NBackGame.kt
+++ nback/src/main/java/ch/instantpastime/nback/core/NBackGame.kt
@@ -5,52 +5,24 @@ import kotlin.random.Random
 
 class NBackGame {
 
-    companion object {
-        const val INDEX_MIN: Int = 0
-        const val INDEX_MAX: Int = 8
-    }
-
+    private val _randomLocation: NBackDraw
+    private val _randomLetter: NBackDraw
     val _letterCount: Int
     val _level: Int
 
     constructor(nbLetters: Int, nBackLevel: Int) {
         _letterCount = if (nbLetters >= 0) nbLetters else 0
         _level = if (nBackLevel > 0) nBackLevel else 1
-        _lastLocations = CircularArray(_level)
-        _lastLetters = CircularArray(_level)
+        _randomLocation = NBackDraw(0 until 9, _level)
+        _randomLetter = NBackDraw(0 until _letterCount, _level)
     }
 
-    private val _lastLocations: CircularArray<Int>
-    private val _lastLetters: CircularArray<Int>
-
     fun getNextIndex(): Pair<Int, Boolean> {
-        val newIndex = Random.nextInt(from = INDEX_MIN, until = INDEX_MAX + 1)
-
-        // Check if there are enough elements in the buffer.
-        val isSame = if (_lastLocations.size() == _level) {
-            val pastIndex = _lastLocations.popFirst()
-            // Check if the past element was the same.
-            pastIndex == newIndex
-        } else {
-            false
-        }
-        _lastLocations.addLast(newIndex)
-        return Pair(newIndex, isSame)
+        return _randomLocation.getNextIndex()
     }
 
     fun getNextLetterIndex(): Pair<Int, Boolean> {
-        val newIndex = Random.nextInt(from = 0, until = _letterCount)
-
-        // Check if there are enough elements in the buffer.
-        val isSame = if (_lastLetters.size() == _level) {
-            val pastIndex = _lastLetters.popFirst()
-            // Check if the past element was the same.
-            pastIndex == newIndex
-        } else {
-            false
-        }
-        _lastLetters.addLast(newIndex)
-        return Pair(newIndex, isSame)
+        return _randomLetter.getNextIndex()
     }
 
 }
