bd4c0d6ac16e1570c88813495de32f0fe2626981
toni789 <43863327+toni789@users.noreply.github.com>
2020-04-11 13:20:56 +0200

    NBack: added BottomNavigationView
    Base on the BottomNavigationView tutorial of Coding in Flow: https://codinginflow.com/tutorials/android/bottomnavigationview
    
    A dependency to com.google.android.material was also added in the main app build.gradle file.

--- nback/src/main/java/ch/instantpastime/nback/NBackActivity.kt
+++ nback/src/main/java/ch/instantpastime/nback/NBackActivity.kt
@@ -2,22 +2,47 @@ package ch.instantpastime.nback
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
 import ch.instantpastime.nback.fragments.NBackFragment
+import kotlinx.android.synthetic.main.activity_nback.*
 
 class NBackActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        android.os.Debug.waitForDebugger()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nback)
-
-        configureAndShowNBackFragment()
+        nav_view.setOnNavigationItemSelectedListener { onNavigationItemSelected(it) }
+        loadFragment(nav_view.selectedItemId)
     }
 
-    private fun configureAndShowNBackFragment() {
-        if (supportFragmentManager.findFragmentById(R.id.nback_fragment_container) == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.nback_fragment_container, NBackFragment())
-                .commit()
+    private fun buildFragment(@IdRes menuItemId: Int): Fragment? {
+        return when (menuItemId) {
+            R.id.navigation_home -> NBackFragment()
+            else -> null
         }
+    }
+
+    private fun loadFragment(@IdRes menuItemId: Int): Boolean {
+        val oldFragment = supportFragmentManager.findFragmentById(R.id.nback_fragment_container)
+        return if (oldFragment != null && nav_view.selectedItemId == menuItemId) {
+            true
+        } else {
+            val newFragment = buildFragment(menuItemId)
+            if (newFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.nback_fragment_container, newFragment)
+                    .commit()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return loadFragment(item.itemId)
     }
 }
