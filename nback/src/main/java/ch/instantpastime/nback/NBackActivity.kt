c8d8d47da44b9f5789af7d76a0c56edaab92d7c1
toni789 <43863327+toni789@users.noreply.github.com>
2020-04-13 13:47:07 +0200

    NBack: handle the back button events

--- nback/src/main/java/ch/instantpastime/nback/NBackActivity.kt
+++ nback/src/main/java/ch/instantpastime/nback/NBackActivity.kt
@@ -7,50 +7,24 @@ import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import ch.instantpastime.nback.fragments.NBackFragment
 import ch.instantpastime.nback.fragments.NBackPreferenceFragment
+import ch.instantpastime.nback.ui.BackStackHelper
 import kotlinx.android.synthetic.main.activity_nback.*
 
 class NBackActivity : AppCompatActivity() {
+
+    private val backStackHelper = BackStackHelper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //android.os.Debug.waitForDebugger()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nback)
-        nav_view.setOnNavigationItemSelectedListener { onNavigationItemSelected(it) }
-        loadFragment(nav_view.selectedItemId)
+        nav_view.setOnNavigationItemSelectedListener { backStackHelper.onNavigationItemSelected(it) }
+        backStackHelper.loadFragment(nav_view.selectedItemId)
     }
 
-    private fun buildFragment(@IdRes menuItemId: Int): Fragment? {
-        return when (menuItemId) {
-            R.id.navigation_home -> NBackFragment()
-            R.id.navigation_option -> NBackPreferenceFragment()
-            else -> null
+    override fun onBackPressed() {
+        if (!backStackHelper.onBackPressed()) {
+            super.onBackPressed()
         }
-    }
-
-    private fun loadFragment(@IdRes menuItemId: Int): Boolean {
-
-        // Check whether the given menu item is already selected and has a valid fragment.
-        return if (nav_view.selectedItemId == menuItemId &&
-            supportFragmentManager.findFragmentById(R.id.nback_fragment_container) != null) {
-            // The requested menu item is already selected and has a valid fragment.
-            true
-        } else {
-            // Build the fragment for the requested menu item.
-            val newFragment = buildFragment(menuItemId)
-            if (newFragment != null) {
-                // Pass the built fragment to the fragment manager.
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.nback_fragment_container, newFragment)
-                    .commit()
-                true
-            } else {
-                // No fragment could be built for the requested menu item.
-                false
-            }
-        }
-    }
-
-    private fun onNavigationItemSelected(item: MenuItem): Boolean {
-        return loadFragment(item.itemId)
     }
 }
