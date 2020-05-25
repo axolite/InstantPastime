a0aba0c300edde4509350b6b20bc48f3cb566948
toni789 <43863327+toni789@users.noreply.github.com>
2020-05-25 23:31:29 +0200

    PlacePhoto: allow to access bitmap directly

--- app/src/main/java/ch/instantpastime/PlacePhoto.kt
+++ app/src/main/java/ch/instantpastime/PlacePhoto.kt
@@ -1,9 +1,13 @@
 package ch.instantpastime
 
+import android.graphics.Bitmap
 import com.google.android.libraries.places.api.net.FetchPhotoResponse
 
 /**
  * Contains an image of a place.
  * Typically a response from Google Place API.
  */
-data class PlacePhoto(val response: FetchPhotoResponse, val info: PlaceInfo)
+data class PlacePhoto(val response: FetchPhotoResponse, val info: PlaceInfo) {
+    val bitmap: Bitmap
+        get() = response.bitmap
+}
