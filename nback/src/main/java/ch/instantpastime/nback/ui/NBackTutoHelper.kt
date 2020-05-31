a2fe77abf31f0a3c5d8783527185ea7c31e61319
toni789 <43863327+toni789@users.noreply.github.com>
2020-05-31 21:02:09 +0200

    NBack: tutorial image

--- nback/src/main/java/ch/instantpastime/nback/ui/NBackTutoHelper.kt
+++ nback/src/main/java/ch/instantpastime/nback/ui/NBackTutoHelper.kt
@@ -8,29 +8,31 @@ import ch.instantpastime.nback.R
 object NBackTutoHelper {
 
     val tuto_slides = intArrayOf(
-    ch.instantpastime.R.layout.activity_start_content01,
-    ch.instantpastime.R.layout.activity_start_content01,
-    ch.instantpastime.R.layout.activity_start_content01
+        ch.instantpastime.R.layout.activity_start_content01,
+        ch.instantpastime.R.layout.activity_start_content01,
+        ch.instantpastime.R.layout.activity_start_content01
     )
+
     /* ******* Images ************************************************ */
     val tuto_images = intArrayOf(
-    R.drawable.tutoslide01,
-    R.drawable.tutoslide02,
-    R.drawable.tutoslide03
+        R.drawable.tutoslide01,
+        R.drawable.tutoslide02,
+        R.drawable.tutoslide03
 
     )
+
     /* ******* Texts ************************************************* */
     val tuto_texts = intArrayOf(
-    R.string.nback_tuto_text1,
-    R.string.nback_tuto_text2,
-    R.string.nback_tuto_text3
+        R.string.nback_tuto_text1,
+        R.string.nback_tuto_text2,
+        R.string.nback_tuto_text3
     )
 
     fun startTutoActivity(context: Context) {
         val intent = Intent(context, StartActivity::class.java)
-        intent.putExtra( "tuto_slides",  tuto_slides )
-        intent.putExtra("tuto_images", tuto_images )
-        intent.putExtra("tuto_texts", tuto_texts )
+        intent.putExtra("tuto_slides", tuto_slides)
+        intent.putExtra("tuto_images", tuto_images)
+        intent.putExtra("tuto_texts", tuto_texts)
         context.startActivity(intent)
     }
 }
