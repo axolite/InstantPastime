b7d9a732fd19ae2a40c642e29b431847d1e1942e
toni789 <43863327+toni789@users.noreply.github.com>
2020-06-01 14:29:47 +0200

    Common app: added the Brain Workshop app in credits

--- app/src/main/java/ch/instantpastime/CreditDialogHelper.kt
+++ app/src/main/java/ch/instantpastime/CreditDialogHelper.kt
@@ -2,6 +2,9 @@ package ch.instantpastime
 
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
@@ -17,6 +20,12 @@ object CreditDialogHelper {
 
         dialog.findViewById<Button>(R.id.credits_close_button).setOnClickListener {
             dialog.dismiss()
+        }
+
+        dialog.findViewById<View>(R.id.credits_brain_workshop_panel).setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("http://brainworkshop.sourceforge.net/"))
+            context.startActivity(browserIntent)
         }
 
         val lp = WindowManager.LayoutParams()
